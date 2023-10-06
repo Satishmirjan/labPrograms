39a706f3fc4fa9db150bafc65bf0f439b0089b79
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 20:21:16 +0000

    To illustrate the use of increment operator (postfix & prefix)

--- 08.positiveOrNegative.c
+++ 08.positiveOrNegative.c
@@ -1,10 +1,4 @@
 /*Using „ternary‟ operator, check whether a given 
 number is Positive or Negative.*/
 #include<stdio.h>
-void main(){
-    int number;
-  printf("Enter The Number:\n");
-scanf("%d",&number);
- (number%2==0)?printf("Given Number Is Even\n"):printf("Given Number Is 0dd\n");
 
-}
---
+++ 12.preAndPostIncrement.c
@@ -0,0 +1,11 @@
+/*) To illustrate the use of increment operator (postfix & 
+prefix).*/
+#include<stdio.h>
+void main(){
+    int number1=5;
+    int number2=5;
+   
+printf("Pre Increment Demonstration:%d\n",++number1);
+printf("Post Increment Demonstration:%d\n",number2++);
+
+}

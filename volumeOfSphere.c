eef69041ecd7f8711f114f20124b2ac12c4fad57
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 18:25:24 +0000

    To find the volume of a Sphere using the formula

---
+++ volumeOfSphere.c
@@ -0,0 +1,12 @@
+/*To find the volume of a Sphere using the formula, 
+Volume = 4 / 3 Π r*r*r */
+#include<stdio.h>
+
+void main(){
+    int radius;
+    float area_of_circle;
+printf("Enter The Radius Of Circle:\n");
+scanf("%d",&radius);
+area_of_circle=(1.33*3.14*radius*radius);
+printf("Area Of The Given Circle : %f",area_of_circle);
+}

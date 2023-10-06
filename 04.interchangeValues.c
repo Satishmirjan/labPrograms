d8f37a86200ad8c969af818a3f2dc284e7a915ae
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 18:36:36 +0000

    write a program to rotate values of x,y,z

--- 04.interchangeValues.c
+++ 04.interchangeValues.c
@@ -7,9 +7,10 @@ void main(){
     int x,y,z,temp;
    
 printf("Enter The Values of x,y,z:\n");
-scanf("%d%d%d",&a,b,c);
+scanf("%d%d%d",&x,&y,&z);
 temp=x;
 x=y;
 y=z;
-printf("value of x:%d\ny:%d\nz:%d",x,y,z);
+z=temp;
+printf("value of x:%d \n y:%d \n z:%d",x,y,z);
 }

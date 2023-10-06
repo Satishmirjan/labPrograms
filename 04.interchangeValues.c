48bd23607d7768d596864cbc2b2a54bd81adc252
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 18:31:54 +0000

    write a program to rotate values of x,y,z

---
+++ 04.interchangeValues.c
@@ -0,0 +1,15 @@
+/* Given the values of the variables x, y and z, write a 
+program to rotate their values such that x has the value 
+of y, y has the value of z, and z has the value of x*/
+#include<stdio.h>
+
+void main(){
+    int x,y,z,temp;
+   
+printf("Enter The Values of x,y,z:\n");
+scanf("%d%d%d",&a,b,c);
+temp=x;
+x=y;
+y=z;
+printf("value of x:%d\ny:%d\nz:%d",x,y,z);
+}

69adcb8cc00f1dade481bafb802cffd5a480a1b5
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 20:28:26 +0000

    Using „ternary‟ operator, check whether a given number is Positive or Negative

--- 08.positiveOrNegative.c
+++ 08.positiveOrNegative.c
@@ -2,3 +2,11 @@
 number is Positive or Negative.*/
 #include<stdio.h>
 
+#include<stdio.h>
+void main(){
+    int number;
+  printf("Enter The Number:\n");
+scanf("%d",&number);
+((number==0)?printf("Given Number Is Neither positive nor negative\n"):((number>0)?printf("Given Number Is positive\n"):printf("Given Number Is Negative")));
+
+}
--- 11.oddOrEven.c
+++ 11.oddOrEven.c
@@ -1,12 +1,11 @@
 /* Using „&‟ operator, check whether a given number is 
 Odd or Even.*/
 
-
 #include<stdio.h>
 void main(){
     int number;
   printf("Enter The Number:\n");
 scanf("%d",&number);
- (number&1==1)?printf("Given Number Is Odd\n"):printf("Given Number Is Even\n");
+(number&1==1)?printf("Given Number Is Odd\n"):printf("Given Number Is Even\n");
 
 }

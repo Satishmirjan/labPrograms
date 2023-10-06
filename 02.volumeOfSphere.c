ea19449586cb95dafce553145b225f5998ec5009
Satishmirjan <123397923+Satishmirjan@users.noreply.github.com>
2023-10-06 19:00:46 +0000

    To find the volume of a Sphere using the formula, Volume = 4 / 3 Π r*r*r

--- 02.volumeOfSphere.c
+++ 02.volumeOfSphere.c
@@ -7,6 +7,6 @@ void main(){
     float area_of_circle;
 printf("Enter The Radius Of Circle:\n");
 scanf("%d",&radius);
-area_of_circle=(1.33*3.14*radius*radius);
+area_of_circle=(1.33*3.14*radius*radius*radius);
 printf("Area Of The Given Circle : %f",area_of_circle);
 }
---
+++ 03.areaOfTriangle.c
@@ -0,0 +1,17 @@
+/*To find the area of a Triangle with 3 sides given using 
+formula, Area = √(s(s-a) (s-b) (s-c)) 
+where a, b and c are lengths of sides of Triangle, and
+s = (a+b+c) / 2*/
+#include<stdio.h>
+#include<math.h>
+
+void main(){
+    int a,b,c;
+    float area_of_traingle,s;
+printf("Enter The Sides of Triangle:\n");
+scanf("%d%d%d",&a,&b,&c);
+s=(a+b+c)/2;
+area_of_traingle=sqrt(s*(s-a)*(s-b)*(s-c));
+
+printf("Area Of The Given Triangle : %f",area_of_traingle);
+}
